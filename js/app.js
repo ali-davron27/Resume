a73e0eecbccd4ce766a36f71dbd672b71ecafa66
Ali <89781790+ali-davron27@users.noreply.github.com>
2022-02-09 23:09:23 +0500

    vue
    veu

--- js/app.js
+++ js/app.js
@@ -1,27 +1,27 @@
 new Vue({ 
 el:"#app",
 data:{
-  name:'Davronov',
-  x:0,
-  y:0 
+  name:'',
+  // x:0,
+  // y:0 
 },
 
 methods:{
 update(){
 setTimeout(()=>{
     this.name = 'ALI'
-},25000)  
+},5000)  
 },
 
 
-changeName(){
-this.name = 'Davoo'
-},
+// changeName(){
+// this.name = 'Davoo'
+// },
 
-graph(event){
-this.x=event.clientX;
-this.y=event.clientY;
-}
+// graph(event){
+// this.x=event.clientX;
+// this.y=event.clientY;
+// }
 
     },
 })
